9fea2237ed9051ab9903c17df32bad2e49bcfdfb
skouroliakos <thanosmw2@gmail.com>
2023-11-24 16:19:50 +0200

    test

--- src/components/shared/LeftSidebar.tsx
+++ src/components/shared/LeftSidebar.tsx
@@ -28,12 +28,7 @@ const LeftSidebar = () => {
     <nav className="leftsidebar">
       <div className="flex flex-col gap-11">
         <Link to="/" className="flex gap-3 items-center">
-          <img
-            //src="/assets/images/logo.svg"
-            alt="logo"
-            width={170}
-            height={36}
-          />
+          <h3>SocialMediaApp</h3>
         </Link>
 
         {isLoading || !user.email ? (
